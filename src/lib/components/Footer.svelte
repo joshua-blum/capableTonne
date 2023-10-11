782b05ee0ae0f538da0e3252900af7fc3e44f5f1
Joshua Blum <blum.r.joshua@gmail.com>
2023-10-10 20:33:36 -0700

    Create footer skeleton

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,60 @@
 <script lang="ts"></script>
 
-<footer class="flex bg-blue-300">
-    Here is a div where the footer will exist
+<footer class="flex bg-blue-300 flex-col">
+    <h2>CapableTonne</h2>
+    <section class="flex flex-row">
+        <div class="mainLinks flex flex-col">
+            <ul>
+                <li>Register Live or Push</li>
+                <li>About CapableTonne</li>
+                <li>Jobs</li>
+                <div class="socials flex flex-row">
+                    insert social images here
+                </div>
+            </ul>
+        </div>
+        <div class="educationLinks flex flex-col">
+            <p>Education</p>
+            <ul>
+                <li>Offers for students and teachers</li>
+                <li>CapableTonne for the classroom</li>
+                <li>CapableTonne for colleges and universities</li>
+            </ul>
+        </div>
+        <div class="newsletter flex flex-col">
+            <h4>Sign up for our newsletter</h4>
+            <p>Enter your email address to stay up to date with the latest offers, tutorials, downloads, surveys and more.
+            </p>
+            <p>insert signup for here</p>
+        </div>
+    </section>
+    <section class="flex flex-row">
+        <div class="communityLinks flex flex-col">
+            <p>Community</p>
+            <ul>
+                <li>Find CapableTonne User Groups</li>
+                <li>Find certified training</li>
+                <li>Become a certified trainer</li>
+            </ul>
+        </div>
+        <div class="langAndLoc flex flex-col">
+            <h4>Language and Location</h4>
+            <p>Dropdown for language | dropdown for location
+            </p>
+        </div>
+    </section>
+    <section class="flex flex-row justify-between">
+        <ul>
+            <li>Contact Us</li>
+            <li>Press Resources</li>
+            <li>Legal Info</li>
+            <li>Privacy Policy</li>
+            <li>Cookie Settings</li>
+            <li>Imprint</li>
+        </ul>
+        <div class="finalStuff">
+            <p>Made in Berlin</p>
+            <p>logo goes here</p>
+        </div>
+    </section>
 </footer>
